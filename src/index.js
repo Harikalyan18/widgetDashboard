1b444b31b2951dbe48d013b28d0d1cc4ab2f2edc
Harikalyan18 <165786039+Harikalyan18@users.noreply.github.com>
2024-12-24 00:44:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <MantineProvider withGlobalStyles withNormalizeCSS>
+  <MantineProvider>
   <App />
 </MantineProvider>
 );
