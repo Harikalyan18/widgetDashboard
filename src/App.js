6d65e248b222c0f02f2c780233e7f45dc8a5065c
Harikalyan18 <165786039+Harikalyan18@users.noreply.github.com>
2024-12-24 00:42:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import SelectLocation from './Components/selectLocation';
-// import VolumetricsWidget from './Components/grid';
-// import SelectCategory from './Components/selectCategory';
-// import WidgetDashboard from './Components/WidgetDashboard';
 import Dashboard from './Components/Dashboard';
 
 function App() {
